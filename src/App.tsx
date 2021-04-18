c35a1d4d994ece11916e66b4742bd67e0ede0834
Erdi Koçak <erdist@mail.com>
2021-04-18 12:54:26 +0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Divider, Form, Grid, Message } from "semantic-ui-react";
+import { useState } from "react";
 
 function App() {
+  const [output, setOutput] = useState("");
+  let vovels = /[aeıioöuü]/i;
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    hyphenate(e.target.value);
+  }
+
+  function isVowel(value: string) {
+    return vovels.test(value);
+  }
+
+  function hyphenate(input: string) {
+    var syllables: string[] = [];
+    var letters: string[] = [];
+    var i = input.length - 1;
+    while (i >= 0) {
+      if (isVowel(input[i])) {
+        letters.push(input[i]);
+        i--;
+        if (i >= 0 && isVowel(input[i]) === false) {
+          letters.push(input[i]);
+          i--;
+        }
+        syllables.push(letters.reverse().join(""));
+        letters = [];
+      } else {
+        letters.push(input[i]);
+        if (i === 0) {
+          syllables[syllables.length - 1] =
+            input[i] + syllables[syllables.length - 1];
+        }
+        i--;
+      }
+    }
+    var result = syllables.reverse().join(" - ");
+    setOutput(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid columns={2} relaxed="very" stackable>
+        <Grid.Column width="8" stretched>
+          <Form widths="equal">
+            <Form.TextArea
+              width="16"
+              label="Your Text"
+              onChange={handleChange}
+              placeholder="Enter your text here..."
+            ></Form.TextArea>
+          </Form>
+        </Grid.Column>
+
+        <Grid.Column width="8" stretched>
+          <Message.List>
+            <Message.Header>Result</Message.Header>
+            <p>{output}</p>
+          </Message.List>
+        </Grid.Column>
+      </Grid>
+
+      <Divider vertical>Turkish Hyphenator</Divider>
     </div>
   );
 }
